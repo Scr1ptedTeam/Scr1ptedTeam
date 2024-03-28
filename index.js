83d8b319dbee8964d1fcd5fa864ff78eb1e81074
Scr1ptedTeam <109403503+Scr1ptedTeam@users.noreply.github.com>
2024-03-28 13:35:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,21 @@
 
 if (window.console==null) { window["console"] = { log : function() {} } }; // some browsers do not set console
 
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    const downloadLink = document.createElement('a');
+    downloadLink.href = 'Apps/App.zip';
+    downloadLink.download = 'App.zip'; // Устанавливаем имя файла для скачивания
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    downloadLink.remove();
+});
+
+
+
+
 var Application = function() {
 	// event constants
 	this.prefix = "--web-";
